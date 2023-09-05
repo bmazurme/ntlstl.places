06b057ac2266a87c712dde0c4b21b53a200369f1
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-05 08:17:30 +0300

    fix: card-route

--- src/server/routes/card-route/index.ts
+++ src/server/routes/card-route/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import {
   createCard,
@@ -6,12 +7,15 @@ import {
   likeCard,
   dislikeCard,
 } from '../../controllers/card-controller';
-import { validateCardData, validateObjectId } from '../../utils/validator';
+import { validateObjectId } from '../../utils/validator';
+// validateCardData,
 import { UrlsApi } from '../../utils/routers';
 
+const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-router.post(UrlsApi.CARDS.INDEX, validateCardData, createCard);
+router.post(UrlsApi.CARDS.INDEX, upload.array('files'), createCard);
+
 router.delete(UrlsApi.CARDS.ID, validateObjectId, deleteCard);
 router.put(UrlsApi.CARDS.LIKES, validateObjectId, likeCard);
 router.delete(UrlsApi.CARDS.LIKES, validateObjectId, dislikeCard);
