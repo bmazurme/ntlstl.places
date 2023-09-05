540aa9d49000958445014c09f037c1412906eaa1
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-05 08:17:08 +0300

    fix: card-controller

--- src/server/controllers/card-controller/index.ts
+++ src/server/controllers/card-controller/index.ts
@@ -1,17 +1,31 @@
 /* eslint-disable consistent-return */
 import { NextFunction, Request, Response } from 'express';
+// import { UploadedFile } from 'express-fileupload';
+import path from 'path';
+import fs from 'fs';
 
 import { BadRequestError, NotFoundError, ForbiddenError } from '../../errors';
 
 import Card from '../../models/card-model';
 import { IUser } from '../../models/user-model';
 
-const createCard = async (req: unknown, res: Response, next: NextFunction) => {
+const createCard = async (req: any, res: Response, next: NextFunction) => {
   try {
-    const { name, link } = (req as Request).body;
-    const card = await Card.create({ name, link, userId: (req as & { user: IUser}).user._id });
+    const { name } = (req as Request).body;
 
-    return res.status(200).send(card);
+    const tempPath = req.files[0].path;
+    const targetPath = path.join('uploads', req.files[0].originalname);
+    // if (path.extname(req.files[0].originalname).toLowerCase()) {
+    fs.rename(tempPath, targetPath, (err) => { console.log('err'); });
+
+    const card = await Card.create({
+      ...req.files[0],
+      name,
+      link: req.files[0].originalname,
+      userId: (req as & { user: IUser}).user._id,
+    });
+
+    return res.send(card);
   } catch (error: unknown) {
     if ((error as Error).name === 'ValidationError') {
       return next(new BadRequestError('переданы некорректные данные в метод'));
