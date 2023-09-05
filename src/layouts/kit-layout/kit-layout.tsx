d34db1237b19ca4128d82065ca176c26f83f18aa
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-05 08:12:26 +0300

    fix: kit-layout

---
+++ src/layouts/kit-layout/kit-layout.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/outline';
+
+import UploadButton from '../../components/upload-button';
+
+import style from './kit-layout.module.css';
+
+export default function NotFoundLayout() {
+  return (
+    <div className={style.container}>
+      <UploadButton onChange={(formData: FormData) => console.log(formData)} />
+
+      <div className={style.counter}>
+        <div className={style.icon}>
+          <EyeIcon className="h-3 w-3" />
+        </div>
+        <span className={style.value}>0</span>
+      </div>
+
+    </div>
+  );
+}
