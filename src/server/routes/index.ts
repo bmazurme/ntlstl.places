466b99414deaddee249871c0ea6166a9cea88931
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-05 08:18:15 +0300

    fix: routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -5,6 +5,7 @@ import oauthRoute from './oauth-route';
 import userRoute from './user-route';
 import cardsRoute from './cards-route';
 import cardRoute from './card-route';
+import fileRoute from './file-route';
 
 import authMiddleware from '../middlewares/auth-middleware';
 
@@ -13,6 +14,7 @@ const router = Router();
 router.use('/', authRoute);
 router.use('/', oauthRoute);
 router.use('/', cardsRoute);
+router.use('/', fileRoute);
 router.use('/', authMiddleware, userRoute);
 router.use('/', authMiddleware, cardRoute);
 
