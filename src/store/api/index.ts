33884a92d99dcf8acbd450044da53a284fef072e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-05 08:14:40 +0300

    fix: api

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -1,7 +1,9 @@
 export { default as authApi } from './auth-api';
 export { default as userApi } from './user-api';
 export { default as cardsApi } from './card-api';
+export { default as filesApi } from './file-api';
 
 export * from './auth-api/endpoints';
 export * from './user-api/endpoints';
 export * from './card-api/endpoints';
+export * from './file-api/endpoints';
