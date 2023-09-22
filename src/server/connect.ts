816e6c1c3941f3b04141866f267a566a4dee4b2f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-23 01:30:00 +0300

    fix: config

--- src/server/connect.ts
+++ src/server/connect.ts
@@ -8,7 +8,7 @@ import Like from './models/like-model';
 dotEnvConfig();
 
 const sequelizeOptions: SequelizeOptions = {
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST ?? 'postgres',
   port: +(process.env.POSTGRES_PORT ?? 5432),
   username: process.env.POSTGRES_USER ?? 'postgres',
   password: process.env.POSTGRES_PASSWORD ?? 'newPassword',
