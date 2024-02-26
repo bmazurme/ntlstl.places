1d35fc7fcaffb9a2dab7ff8ad4542b8ef4d0b23c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-02-26 19:51:26 +0300

    fix: index server

--- src/server/index.ts
+++ src/server/index.ts
@@ -20,7 +20,7 @@ import limiter from './utils/limiter';
 
 import dbConnect from './connect';
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3005;
 
 const app = express();
 
