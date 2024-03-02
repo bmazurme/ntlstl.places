200490fd3dd7120416ac9e1c75ab5c22cbfc35c0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-03-02 16:43:03 +0300

    fix: slide

--- src/components/slide/slide.tsx
+++ src/components/slide/slide.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { useAppSelector } from '../../hooks';
 import { cardSelector } from '../../store';
+
+import { MODAL_CONFIG } from '../../utils';
 
 import style from './slide.module.css';
 
@@ -9,9 +12,23 @@ export default function Slide() {
   const card = useAppSelector(cardSelector);
 
   return (
-    <div className={style.slide}>
-      <img src={`/api/files/${card?.link}` ?? ''} alt={card?.name ?? ''} className={style.image} />
-      <p className={style.name}>{card?.name ?? ''}</p>
-    </div>
+    <AnimatePresence>
+      <div className={style.slide}>
+        <motion.img
+          initial={{
+            opacity: 0,
+            scale: 0.175,
+          }}
+          animate={MODAL_CONFIG.ANIMATE}
+          exit={MODAL_CONFIG.EXIT}
+          src={`/api/files/${card?.link}` ?? ''}
+          alt={card?.name ?? ''}
+          className={style.image}
+          height="100%"
+          width="100%"
+        />
+        <p className={style.name}>{card?.name ?? ''}</p>
+      </div>
+    </AnimatePresence>
   );
 }
