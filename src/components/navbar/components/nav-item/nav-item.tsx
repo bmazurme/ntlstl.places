52d9694ff14d97d7a67029ddc337c86b0f1b5e8f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-03-09 10:26:41 +0300

    fix: nav-item

--- src/components/navbar/components/nav-item/nav-item.tsx
+++ src/components/navbar/components/nav-item/nav-item.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 
 import style from './navbar-item.module.css';
 
 export interface INavItemProps {
-  to: string,
+  to: string;
   className?: string;
-  onClick?: () => void,
-  active: string,
-  value: string,
+  onClick?: () => void;
+  value: string;
 }
 
 export default function NavItem({
-  to, onClick, className, active, value,
+  to, onClick, className, value,
 }: INavItemProps) {
   return (
     <li className={style.item}>
-      <Link
+      <NavLink
         to={to}
         onClick={onClick}
-        className={classNames(className, style.link, { [style.active]: active })}
+        className={({ isActive }) => classNames(
+          className,
+          style.link,
+          { [style.active]: isActive },
+        )}
       >
         {value}
-      </Link>
+      </NavLink>
     </li>
   );
 }
