f96a83546bffbfd68eefe62d09f392be7d9d30dd
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-03-19 08:01:45 +0300

    fix: routes moved

--- backend/src/routes/card.route.ts
+++ backend/src/routes/card.route.ts
@@ -9,6 +9,7 @@ import {
   updateCard,
 } from '../controllers/card.controller';
 import { UrlsApi } from '../utils/routers';
+import { updateFiles } from '../controllers/file.controller';
 
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
@@ -18,5 +19,6 @@ router.delete(UrlsApi.CARDS.ID, deleteCard);
 router.patch(UrlsApi.CARDS.ID, updateCard);
 router.put(UrlsApi.CARDS.LIKES, likeCard);
 router.delete(UrlsApi.CARDS.LIKES, dislikeCard);
+router.post(UrlsApi.FILES.UPDATE, updateFiles);
 
 export default router;
