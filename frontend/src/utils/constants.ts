076417130664baf1101765b00ca449879833d464
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-03-22 11:45:34 +0300

    fix

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -23,3 +23,5 @@ export const Regexp = {
   EMAIL: '[a-z0-9._%+-]+@[a-z0-9.-]+[\\.{0}][a-z]{2,3}$',
 };
 export const BASE_API_URL = process.env.API_HOST;
+// eslint-disable-next-line prefer-destructuring
+export const YA_ENDPOINT = process.env.YA_ENDPOINT;
