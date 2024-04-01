b4b480e8b655a34dbccfc7b7cb556501d6cb2a6c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-01 07:57:29 +0300

    fix: cypress config

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+import { defineConfig } from 'cypress';
+import { config as dotEnvConfig } from 'dotenv';
+
+dotEnvConfig();
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3005/',
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+    },
+  },
+});
