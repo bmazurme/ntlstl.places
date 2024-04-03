d00be3782a139fc8ac3c72d75a25d58ce0f350a4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-03 08:31:49 +0300

    feat: cy tests

---
+++ frontend/cypress/e2e/profile.cy.ts
@@ -0,0 +1,29 @@
+describe('app works correctly with routes', () => {
+  before(() => {
+    cy.visit('/');
+    cy.viewport(1800, 1200);
+    const name = Cypress.env('COOKIE_NAME');
+    const value = Cypress.env('COOKIE_VALUE');
+    cy.setCookie(name, value);
+  });
+
+  it('profile', () => {
+    cy.contains('[ntlstl.places]');
+    cy.contains('mesto by ntlstl');
+    cy.contains('Telegram');
+    cy.contains('LinkedIn');
+    cy.contains('Users');
+    cy.contains('Sign Out');
+
+    cy.visit('/users');
+    cy.contains('Users');
+
+    cy.visit('/users/1');
+    cy.contains('Posts');
+
+    cy.visit('/404');
+    cy.contains('404');
+    cy.contains('Page not found');
+    cy.contains('Home');
+  });
+});
---
+++ frontend/cypress/e2e/router-mobile.cy.ts
@@ -0,0 +1,22 @@
+describe('app works correctly with routes', () => {
+  before(() => {
+    cy.visit('/');
+    cy.viewport(412, 915);
+  });
+
+  it('main to 404 & signin', () => {
+    cy.contains('[ntlstl.places]');
+    cy.contains('mesto by ntlstl');
+    cy.contains('Telegram');
+    cy.contains('LinkedIn');
+    cy.contains('Sign in');
+
+    cy.visit('/404');
+    cy.contains('404');
+    cy.contains('Page not found');
+    cy.contains('Home');
+
+    cy.visit('/signin');
+    cy.contains('Sign In');
+  });
+});
