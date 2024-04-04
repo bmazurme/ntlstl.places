087d9708b922234de872ba2116383718dc11bbcd
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-04 07:44:42 +0300

    fix: oauth-page

--- frontend/src/pages/oauth-page/oauth-page.tsx
+++ frontend/src/pages/oauth-page/oauth-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import OauthLayout from '../../layouts/oauth-layout';
+import Content from '../../components/content';
 
 import withUser from '../../hocs/with-user';
 
 function OauthPage() {
-  return (<OauthLayout />);
+  return (<Content children={<OauthLayout />} />);
 }
 
 export default withUser(OauthPage, false);
