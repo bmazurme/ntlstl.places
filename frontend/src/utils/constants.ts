1eec08a80bfee568eba2bd6ccc6ceebd67ed1282
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-04 07:46:21 +0300

    fix: app add 500 page

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -25,6 +25,7 @@ export const Urls = {
     INDEX: '/kit',
   },
   404: '*',
+  500: '/internal-server-error',
 };
 export const Regexp = {
   EMAIL: '[a-z0-9._%+-]+@[a-z0-9.-]+[\\.{0}][a-z]{2,3}$',
