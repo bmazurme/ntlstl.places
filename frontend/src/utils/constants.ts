941b25db2c7cc98683150fd896b43c295c01f8a3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-06 19:47:47 +0300

    fix: constants - add base-host-url

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -31,5 +31,6 @@ export const Regexp = {
   EMAIL: '[a-z0-9._%+-]+@[a-z0-9.-]+[\\.{0}][a-z]{2,3}$',
 };
 export const BASE_API_URL = process.env.API_HOST;
+export const BASE_HOST_URL = process.env.HOST;
 // eslint-disable-next-line prefer-destructuring
 export const YA_ENDPOINT = process.env.YA_ENDPOINT;
