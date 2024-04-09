ebd663a538976d29342389145975c7677095701f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-09 22:05:53 +0300

    fix: backend routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,7 +6,6 @@ import userRoute from './users.route';
 import cardsRoute from './cards.route';
 import cardRoute from './card.route';
 import fileRoute from './files.route';
-import likeRoute from './likes.route';
 import tagRoute from './tags.route';
 
 import authMiddleware from '../middlewares/auth-middleware';
@@ -18,7 +17,6 @@ router.use(authRoute);
 router.use(oauthRoute);
 router.use(userMiddleware, cardsRoute);
 router.use(fileRoute);
-router.use(likeRoute);
 router.use(authMiddleware, cardRoute); // move to component
 router.use(authMiddleware, userRoute); // move to component
 router.use(authMiddleware, tagRoute); // move to component
