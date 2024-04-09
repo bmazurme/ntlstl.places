31cb6c188d1f31e288bde8eed882f40d8f0b5863
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-09 22:06:31 +0300

    fix: store

--- frontend/src/store/slices/index.ts
+++ frontend/src/store/slices/index.ts
@@ -2,6 +2,4 @@ export * from './user-slice';
 export * from './users-slice';
 export * from './card-slice';
 export * from './cards-slice';
-export * from './current-slice';
 export * from './user-cards-slice';
-export * from './user-current-slice';
