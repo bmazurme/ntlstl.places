a6fcca91205261e14ada4d382444f6bebfc5c1ab
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-12 07:40:19 +0300

    fix: cards uuid

--- frontend/src/components/cards/cards.tsx
+++ frontend/src/components/cards/cards.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import InfiniteScroll from 'react-infinite-scroller';
 
@@ -16,7 +17,7 @@ export default function Cards({ fetchItems, hasMoreItems, cards }
       loader={<CardLoader key="loader" />}
     >
       <section className={style.cards}>
-        {cards.map((card, i) => (<Card key={card?.id} card={card} index={i} />))}
+        {cards.map((card, i) => (<Card key={uuidv4()} card={card} index={i} />))}
       </section>
     </InfiniteScroll>
   );
