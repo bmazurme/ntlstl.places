3896dc58f96164190c36099eb9df8ec412696a14
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-13 12:06:17 +0300

    fix: tag-controller

--- backend/src/controllers/tag.controller.ts
+++ backend/src/controllers/tag.controller.ts
@@ -1,10 +1,11 @@
 /* eslint-disable consistent-return */
 import { NextFunction, Request, Response } from 'express';
 
-import { BadRequestError } from '../errors';
+import { BadRequestError, ForbiddenError, NotFoundError } from '../errors';
 
 import Tag from '../models/tag.model';
-import CardTag from '../models/card-tag.model';
+
+type User = { id: number; };
 
 export const createTag = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -12,21 +13,6 @@ export const createTag = async (req: Request, res: Response, next: NextFunction)
     const tag = await Tag.create({ name });
 
     return res.status(201).send(tag);
-  } catch (error: unknown) {
-    if ((error as Error).name === 'CastError') {
-      return next(new BadRequestError('переданы некорректные данные в метод'));
-    }
-
-    next(error);
-  }
-};
-
-export const bindCardAndTag = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { cardId, tagId } = req.body;
-    const link = await CardTag.create({ cardId, tagId });
-
-    return res.status(201).send(link);
   } catch (error: unknown) {
     if ((error as Error).name === 'CastError') {
       return next(new BadRequestError('переданы некорректные данные в метод'));
@@ -47,5 +33,60 @@ export const getTags = async (req: Request, res: Response, next: NextFunction) =
     }
 
     next(error);
+  }
+};
+
+export const deleteTag = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id: tagId } = req.params;
+    const { id: userId } = (req as Request & { user: User }).user;
+
+    if (userId !== 1) {
+      return next(new ForbiddenError('access denied'));
+    }
+
+    if (Number.isNaN(+tagId)) {
+      return next(new BadRequestError('переданы некорректные данные в метод'));
+    }
+
+    const tag = await Tag.findOne({ where: { id: tagId } });
+
+    if (!tag) {
+      return new NotFoundError('tag was not find');
+    }
+
+    await Tag.destroy({ where: { id: tagId } });
+
+    return res.status(200).send({ message: 'card was deleted', id: (req as Request).params.id });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateTag = async (req: Request, res: Response, next: NextFunction) => {
+  const { id: tagId } = req.params;
+  const { name } = req.body;
+  const { id: userId } = (req as Request & { user: User }).user;
+
+  if (userId !== 1) {
+    return next(new ForbiddenError('access denied'));
+  }
+
+  if (Number.isNaN(+tagId)) {
+    return next(new BadRequestError('переданы некорректные данные в метод'));
+  }
+
+  try {
+    const tag = await Tag.findOne({ where: { id: tagId } });
+
+    if (!tag) {
+      return new NotFoundError('card was not found');
+    }
+
+    await tag.update({ name });
+
+    return res.status(200).send({ id: tag.id, name: tag.name });
+  } catch (err) {
+    next(err);
   }
 };
