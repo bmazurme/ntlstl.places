7cea0e7aa4ee07caf15ddfb762a14db683f20945
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-15 21:27:06 +0300

    fix: store refactoring

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -69,7 +69,6 @@ export const history = createReduxHistory(store);
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
