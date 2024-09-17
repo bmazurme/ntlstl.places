d27e7110e32b8df0f8d847f22d11b77dd5026e3d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-09-17 07:42:21 +0300

    fix: ui

--- frontend/src/ui/index.ts
+++ frontend/src/ui/index.ts
@@ -3,8 +3,10 @@ import IconButton from './icon-button';
 import InputField from './input-field';
 import Popup from './modal';
 import UploadButton from './upload-button';
+import Preloader from './preloader';
 
 export {
   Button, IconButton, InputField, Popup, UploadButton,
+  Preloader,
 };
 export type { InputProps } from './input-field';
